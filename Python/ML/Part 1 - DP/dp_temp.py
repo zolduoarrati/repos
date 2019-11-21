4aab6dca506cef9ab32ea278712f5b67b0ed7307
Elijah Zolduoarrati <zolduoarrati@gmail.com>
2019-11-21 13:41:37 +1300

    ml/dp/importing_dataset_pandas + creating dep/indep var vector

---
+++ Python/ML/Part 1 - DP/dp_temp.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 21 12:42:55 2019
+author: Elijah Zolduoarrati
+"""
+# importing libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#importing a dataset
+dataset = pd.read_csv('Data.csv')
