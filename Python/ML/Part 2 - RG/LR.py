56526cc26321c68661b3df3df9509654765d33b4
Elijah Zolduoarrati <zolduoarrati@gmail.com>
2019-11-22 10:52:01 +1300

    train and test-regression object_regressor

--- Python/ML/Part 2 - RG/LR.py
+++ Python/ML/Part 2 - RG/LR.py
@@ -6,14 +6,16 @@ author: Elijah Zolduoarrati
 # importing libraries
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as ptl
 # importing files
 dataset = pd.read_csv('Salary_Data.csv')
 # data manipulation
-x = dataset.iloc[:,1].values
-y = dataset.iloc[:,0].values
+x = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,1].values
 # creating train and test data
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.2,random_state=0)
-
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 1/3,random_state=0)
+# fitting linear regression to the training set
+regressor = LinearRegression().fit(x_train, y_train)
 
 
