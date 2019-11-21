d711f16a21a8302a9afdbf4b7660cee1b63d6048
Elijah Zolduoarrati <zolduoarrati@gmail.com>
2019-11-22 10:14:15 +1300

    train and test

--- Python/ML/Part 2 - RG/LR.py
+++ Python/ML/Part 2 - RG/LR.py
@@ -5,13 +5,15 @@ author: Elijah Zolduoarrati
 """
 # importing libraries
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 # importing files
 dataset = pd.read_csv('Salary_Data.csv')
 # data manipulation
 x = dataset.iloc[:,1].values
 y = dataset.iloc[:,0].values
 # creating train and test data
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.2,random_state=0)
 
 
 
