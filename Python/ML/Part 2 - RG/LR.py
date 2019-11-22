0508e2f017dce19ff8bef5ff0d0fb8912a1f1b0f
Elijah Zolduoarrati <zolduoarrati@gmail.com>
2019-11-22 13:33:42 +1300

    visiuals

--- Python/ML/Part 2 - RG/LR.py
+++ Python/ML/Part 2 - RG/LR.py
@@ -7,7 +7,7 @@ author: Elijah Zolduoarrati
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-import matplotlib.pyplot as ptl
+import matplotlib.pyplot as plt
 # importing files
 dataset = pd.read_csv('Salary_Data.csv')
 # data manipulation
@@ -19,4 +19,11 @@ x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 1/3,random_stat
 regressor = LinearRegression().fit(x_train, y_train)
 # predicting the test set results
 y_pred = regressor.predict(x_test)
+# visiualising training and predicting set
+plt.scatter(x_train,y_train, color = 'red')
+plt.plot (x_train,regressor.predict(x_train), color = 'blue')
+plt.title('salary vs. experience (training set)')
+plt.xlabel('years of experience')
+plt.ylabel('salary')
+plt.show()
 
