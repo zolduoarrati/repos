589fc689a51dd46759acacf69a7fb9ca551905be
Elijah Zolduoarrati <zolduoarrati@gmail.com>
2019-12-03 11:44:17 +1300

    calc aboutme var len

--- Python/ML/Part 2 - RG/LR.py
+++ Python/ML/Part 2 - RG/LR.py
@@ -19,11 +19,17 @@ x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 1/3,random_stat
 regressor = LinearRegression().fit(x_train, y_train)
 # predicting the test set results
 y_pred = regressor.predict(x_test)
-# visiualising training and predicting set
+# visiualising training set
 plt.scatter(x_train,y_train, color = 'red')
 plt.plot (x_train,regressor.predict(x_train), color = 'blue')
 plt.title('salary vs. experience (training set)')
 plt.xlabel('years of experience')
 plt.ylabel('salary')
 plt.show()
-
+# visiualising testing set
+plt.scatter(x_test,y_test, color = 'red')
+plt.plot (x_train,regressor.predict(x_train), color = 'blue')
+plt.title('salary vs. experience (training set)')
+plt.xlabel('years of experience')
+plt.ylabel('salary')
+plt.show()
---
+++ Python/ML/Part 2 - RG/LR_K.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 22 14:14:23 2019
+Author: Elijah Zolduoarrati
+"""
+#importing libraries
+import os
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+import tensorflow as tf
+import keras
+from keras.models import Sequential,Model,Input
+from keras.layers import Dense 
+# from Keras.callbacks import TensorBoard
+# importing files
+
+# data manipulation
+
+
